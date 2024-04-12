7f2fff1907bea2da853108e8e69330d0ac72a01d
Kurtis Toledo <kurtistoledo@gmail.com>
2024-04-12 15:19:09 -0500

    Task1_1

--- Task1_1.py
+++ Task1_1.py
@@ -1,14 +1,13 @@
-# Given list of grades
+# Lesson 3: Assignments: Python Lists
+# 1. Python List Transformation
 grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
 
-# Task 1: Sort the grades in descending order
-grades_sorted = sorted(grades, reverse=True)
-print("Sorted grades (descending order):", grades_sorted)
 
-# Task 2: Calculate the average grade
-average_grade = sum(grades) / len(grades)
-print("Average grade:", average_grade)
+grades_sort = sorted(grades, reverse=True)
+print("Sorted grades (order going down):", grades_sort)
 
-# Task 3: Replace grades below 80 with "Failed"
+avg_grade = sum(grades) / len(grades)
+print("Avg grade:", avg_grade)
+
 grades_replaced = ['Failed' if grade < 80 else grade for grade in grades]
 print("Grades with failed replaced:", grades_replaced)
